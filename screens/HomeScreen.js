bdf9b2d6e5d468ec8a5e24b9e00f50664e9e639d
Jakub Widurek <jakubwidurek@gmail.com>
2023-07-22 15:43:29 +0200

    create search bar

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,12 @@
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, TextInput } from 'react-native';
 import React, { useLayoutEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
+import { ChevronDownIcon, SearchIcon } from 'react-native-heroicons/solid';
+import {
+  UserIcon,
+  MagnifyingGlassIcon,
+  AdjustmentsVerticalIcon,
+} from 'react-native-heroicons/outline';
 
 import { SafeAreaView } from 'react-native-safe-area-context';
 
@@ -14,14 +20,31 @@ const HomeScreen = () => {
   }, []);
 
   return (
-    <SafeAreaView>
-      <Text className='text-red-500'>sdfsdf</Text>
-
-      <View>
+    <SafeAreaView className='bg-white pt-5'>
+      <View className='flex-row px-4 my-4 items-center space-x-2'>
         <Image
           source={{ uri: 'https://links.papareact.com/wru' }}
           className='h-7 w-7 bg-gray-300 p-4 rounded-full'
         />
+        <View className='flex-1'>
+          <Text className='font-bold text-gray-400 text-xs'>Deliver Now!</Text>
+          <Text className='font-bold text-xl'>
+            Current Location
+            <ChevronDownIcon size={20} color='#00CCBB' />
+          </Text>
+        </View>
+        <UserIcon size={35} color='#00CCBB' />
+      </View>
+      <View className='flex-row items-center space-x-2 pb-2 mx-4'>
+        <View className='flex-row flex-1 bg-gray-200 p-3 space-x-2'>
+          <MagnifyingGlassIcon color='#00CCBB' />
+          <TextInput
+            className='flex-1'
+            placeholder='Restaurants and cousines'
+            keyboardType='default'
+          />
+        </View>
+        <AdjustmentsVerticalIcon size={30} color='#00CCBB' />
       </View>
     </SafeAreaView>
   );
